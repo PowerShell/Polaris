9b0493f20a64047656b7591ea81e726ca4ad1a9d
Tyler Leonhardt <tylerl0706@gmail.com>
2017-10-17 21:23:30 -0700

    add pass/fail

--- test/InvokePester.ps1
+++ test/InvokePester.ps1
@@ -1,1 +1,10 @@
-Invoke-Pester
+# Determine whether the build passed
+try {
+    # this throws if there was an error
+    Invoke-Pester
+    $result = "PASS"
+}
+catch {
+    $resultError = $_
+    $result = "FAIL"
+}
