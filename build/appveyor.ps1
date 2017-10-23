f228945d91f7f535d55c6c5c50b2b427c21dbbb7
Tyler Leonhardt <tylerl0706@gmail.com>
2017-10-23 16:08:49 -0700

    run pester in windows powershell with net451

--- build/appveyor.ps1
+++ build/appveyor.ps1
@@ -1,3 +1,4 @@
+Install-Module Pester
 $res = Invoke-Pester -Path ..\test -OutputFormat NUnitXml -OutputFile TestsResults.xml -PassThru;
 (New-Object System.Net.WebClient).UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\TestsResults.xml));
 if ($res.FailedCount -gt 0) { throw "$($res.FailedCount) tests failed."}
