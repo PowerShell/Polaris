616556f696353085d41965527ed1d67e296387c8
Tyler James Leonhardt <tylerl0706@gmail.com>
2017-11-13 13:06:40 -0800

    fix example that had a bad character in it (#72)

--- example/example.ps1
+++ example/example.ps1
@@ -1,18 +1,18 @@
-if(-not (Test-Path -Path "..\Polaris.psm1")) {
+if(-not (Test-Path -Path ..\Polaris.psm1)) {
     Write-Error -Message "Cannot find Polaris.psm1"
     return
 }
 
 # Import Polaris
-Import-Module –Name ..\Polaris.psm1
+Import-Module -Name ..\Polaris.psm1
 
 # Hello World passing in the Path, Method & ScriptBlock
-New-WebRoute -Path "/helloworld" -Method "GET" -ScriptBlock {
+New-WebRoute -Path /helloworld -Method GET -ScriptBlock {
     $response.Send('Hello World')
 }
 
 # Query Parameters are supported
-New-WebRoute -Path "/hellome" -Method "GET" -ScriptBlock {
+New-WebRoute -Path /hellome -Method GET -ScriptBlock {
     if ($request.QueryParameters['name']) {
         $response.Send('Hello ' + $request.QueryParameters['name'])
     } else {
@@ -30,10 +30,10 @@ $sbWow = {
 }
 
 # Supports helper functions for Get, Post, Put, Delete
-New-PostRoute -Path "/wow" -ScriptBlock $sbWow
+New-PostRoute -Path /wow -ScriptBlock $sbWow
 
 # Body Parameters are supported if you use the -UseJsonBodyParserMiddleware
-New-PostRoute -Path "/hello" -ScriptBlock {
+New-PostRoute -Path /hello -ScriptBlock {
     if ($request.Body.Name) {
         $response.Send('Hello ' + $request.Body.Name);
     } else {
@@ -42,10 +42,10 @@ New-PostRoute -Path "/hello" -ScriptBlock {
 }
 
 # Pass in script file
-New-WebRoute -Path "/example" -Method "GET" -ScriptPath .\script.ps1
+New-WebRoute -Path /example -Method GET -ScriptPath .\script.ps1
 
 # Also support static serving of a directory
-New-StaticRoute -FolderPath "./static" -RoutePath "/public"
+New-StaticRoute -FolderPath ./static -RoutePath /public
 
 # Start the server
 $app = Start-Polaris -Port 8082 -MinRunspaces 1 -MaxRunspaces 5 -UseJsonBodyParserMiddleware -Verbose # all params are optional
