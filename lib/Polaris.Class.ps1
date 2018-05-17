4d0a9e2e08c722ebdbf074898a07ce56688e1d06
Micah Rairdon <Micah.rairdon@gmail.com>
2018-05-16 22:17:42 -0400

    Returning check for platformID

--- lib/Polaris.Class.ps1
+++ lib/Polaris.Class.ps1
@@ -246,7 +246,7 @@ class Polaris {
         }
 
         $this.Listener.IgnoreWriteExceptions = $true
-        if($this.Listener | Get-Member -Name TimeoutManager){
+        if([System.Environment]::OSVersion.Platform -eq [System.PlatformID]::Win32NT -and ($this.Listener | Get-Member -Name TimeoutManager)){
             $this.Listener.TimeoutManager.RequestQueue = [timespan]::FromMinutes(5)
             $this.Listener.TimeoutManager.IdleConnection = [timespan]::FromSeconds(45)
             $this.Listener.TimeoutManager.EntityBody = [timespan]::FromSeconds(50)
