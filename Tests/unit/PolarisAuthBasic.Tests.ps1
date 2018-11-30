c090beed850452ebae74c100220e84770b816916
Jeremy McGee <jeremymcgee6@gmail.com>
2018-11-30 16:02:19 -0700

    Split tests into tests for Errors and Basic Auth.

---
+++ Tests/unit/PolarisAuthBasic.Tests.ps1
@@ -0,0 +1,72 @@
+#
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for full license information.
+#
+
+Describe "Test Polaris Basic Auth" {
+    
+    BeforeAll {
+        Start-Job -Scriptblock {
+            Import-Module C:\scripts\Polaris\Polaris.psd1
+
+            New-PolarisRoute -Path /helloworld -Method GET -Scriptblock {
+                $Response.Send('Hello World')
+            }
+
+            $Polaris = Start-Polaris -MinRunspaces 1 -MaxRunspaces 5 -Auth Basic
+
+            New-PolarisRoute -Method GET -Path '/' -Force -Scriptblock {
+                $ResponseJson = @{
+                    Username = $Request.User.identity.Name
+                    Password = $Request.User.identity.Password
+                } | ConvertTo-Json
+                
+                $Response.SetContentType("application/json")
+                $Response.Send($ResponseJson)
+            }
+
+            # Keeping the job running while the tests are running
+            while ($Polaris.Listener.IsListening) {
+                Wait-Event callbackeventbridge.callbackcomplete
+            }
+        }
+
+        # Give the Polaris Server time to start up in the job
+        Start-Sleep -Seconds 5
+    }    
+    
+
+    Context "Test Basic Auth provider" {
+        function Test-BasicAuth {
+            
+            [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingConvertToSecureStringWithPlainText", "")]
+            [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingUserNameAndPassWordParams", "")]
+            [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingPlainTextForPassword", "")]
+            param ($Username, $Password)
+            
+            It ("Username {0} and Password {1} should match request" -f $Username, $Password) {
+                $EncryptedPass = $Password | ConvertTo-SecureString -asPlainText -Force
+                $Credential = New-Object System.Management.Automation.PSCredential($Username, $EncryptedPass)                
+
+                $Result = Invoke-RestMethod -Uri "http://localhost:8080" -Method Get -UseBasicParsing -Credential $Credential
+                $Result.Username | Should Be $Username
+                $Result.Password | Should Be $Password
+            }
+        }
+
+        $testCases = @(
+            @{ Username = 'blah'; Password = 'hellothere'}
+            @{ Username = 'username'; Password = 'username'}
+            @{ Username = 'jeremym'; Password = 'asdasdf@2232!!*%#23'}
+            @{ Username = '!'; Password = '@'}
+            @{ Username = 'reallong'; Password = 'a!a!a!a!a!a!a!a!a!a!a!a!a!a!a!a!a!a!a!a!a!a!a!a!a!a!a!a!a!a!a!'}
+        )
+
+        foreach ($testCase in $testCases) {
+            Test-BasicAuth @testCase
+        }        
+    }                   
+    AfterAll {
+        Get-Job | Stop-Job | Remove-Job
+    }
+}
--- Tests/unit/PolarisAuthError.Tests.ps1
+++ Tests/unit/PolarisAuthError.Tests.ps1
@@ -5,60 +5,59 @@
 
 Import-Module -Name $PSScriptRoot\..\..\Polaris.psd1
 
-Describe "Test webserver Auth use" {
-    Context "Test starting server with Windows Auth on Linux/Mac" {
-        if (($IsMacOS -or $IsLinux)) {
-            It "Test Anonymous auth on Mac/Linux" {
+Describe "Test Polaris Auth Errors" {
+    Context "Test operating specific errors" {
+        if ($IsMacOS -or $IsLinux) {
+            It "Should not throw an Error with Anonymous auth on Mac/Linux" {
                 { Start-Polaris -Auth Anonymous } | Should not Throw
                 Stop-Polaris
             }
-            It "Test Basic auth on Mac/Linux" {
+            It "Should not throw an Error with Basic auth on Mac/Linux" {
                 { Start-Polaris -Auth Basic } | Should not Throw
                 Stop-Polaris
             }
-            It "Test IntegratedWindowsAuthentication auth on Mac/Linux" {
+            It "Should throw an Error with IntegratedWindowsAuthentication auth on Mac/Linux" {
                 { Start-Polaris -Auth IntegratedWindowsAuthentication } | Should Throw
                 Stop-Polaris
             }   
-            It "Test Digest auth on Mac/Linux" {
+            It "Should throw an Error with Digest auth on Mac/Linux" {
                 { Start-Polaris -Auth Digest } | Should Throw
                 Stop-Polaris
             }
-            It "Test DigeNegotiatest auth on Mac/Linux" {
+            It "Should throw an Error with Negotiate auth on Mac/Linux" {
                 { Start-Polaris -Auth Negotiate } | Should Throw
                 Stop-Polaris
             }
-            It "Test NTLM auth on Mac/Linux" {
+            It "Should throw an Error with NTLM auth on Mac/Linux" {
                 { Start-Polaris -Auth NTLM } | Should Throw
                 Stop-Polaris
             }
         }
         else {
-            It "Test Anonymous auth on Windows" {
+            It "Should not throw an Error with Anonymous auth on Windows" {
                 { Start-Polaris -Auth Anonymous } | Should not Throw
                 Stop-Polaris
             }
-            It "Test Basic auth on Windows" {
+            It "Should not throw an Error with Basic auth on Windows" {
                 { Start-Polaris -Auth Basic } | Should not Throw
                 Stop-Polaris
             }
-            It "Test IntegratedWindowsAuthentication auth on Windows" {
+            It "Should not throw an Error with IntegratedWindowsAuthentication auth on Windows" {
                 { Start-Polaris -Auth IntegratedWindowsAuthentication } | Should not Throw
                 Stop-Polaris
             }   
-            It "Test Digest auth on Windows" {
+            It "Should not throw an Error with Digest auth on Windows" {
                 { Start-Polaris -Auth Digest } | Should not Throw
                 Stop-Polaris
             }
-            It "Test DigeNegotiatest auth on Windows" {
+            It "Should not throw an Error with Negotiate auth on Windows" {
                 { Start-Polaris -Auth Negotiate } | Should not Throw
                 Stop-Polaris
             }
-            It "Test NTLM auth on Windows" {
+            It "Should not throw an Error with NTLM auth on Windows" {
                 { Start-Polaris -Auth NTLM } | Should not Throw
                 Stop-Polaris
             }
         }
-    }
-    
+    }   
 }
