b44e9493b8d8120b4688d3400a8116811435ff5e
Tyler Leonhardt <tylerl0706@gmail.com>
2019-07-25 23:35:57 -0700

    badge and remove powershellget logic

--- .vsts-ci/azurePipelinesBuild.ps1
+++ .vsts-ci/azurePipelinesBuild.ps1
@@ -1,14 +1,6 @@
 $ErrorActionPreference = 'Stop'
 
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted | Out-Null
-if ($IsWindows -or $PSVersionTable.PSVersion.Major -lt 6) {
-    # Just in case, let's get the latest PowerShellGet.
-    Get-Module PowerShellGet,PackageManagement | Remove-Module -Force -Verbose
-    powershell -Command { Install-Module -Name PowerShellGet -MinimumVersion 1.6 -Force -Confirm:$false -Verbose }
-    powershell -Command { Install-Module -Name PackageManagement -MinimumVersion 1.1.7.0 -Force -Confirm:$false -Verbose }
-    Import-Module -Name PowerShellGet -MinimumVersion 1.6 -Force
-    Import-Module -Name PackageManagement -MinimumVersion 1.1.7.0 -Force
-}
 
 # Needed for build and docs gen eventually...
 Install-Module PlatyPS -Scope CurrentUser -Force
