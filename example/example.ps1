94b5178cff36e344f8bbee1f96f9798b964f7890
xtqqczze <xtqqczze@gmail.com>
2020-08-09 22:08:35 +0100

    Update copyright header

--- example/example.ps1
+++ example/example.ps1
@@ -1,7 +1,5 @@
-#
-# Copyright (c) Microsoft. All rights reserved.
-# Licensed under the MIT license. See LICENSE file in the project root for full license information.
-#
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
 
 if(-not (Test-Path -Path ..\Polaris.psd1)) {
     Write-Error -Message "Cannot find Polaris.psd1"
